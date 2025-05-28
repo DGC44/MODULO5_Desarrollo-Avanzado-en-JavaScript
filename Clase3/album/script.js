d9532995506b2dfb50452dee6bc5238fe16069ad
DianaGC <garcapdia@hotmail.com>
2025-05-27 20:12:16 -0600

    Clase 3

---
+++ Clase3/album/script.js
@@ -0,0 +1,58 @@
+const imagesContainer = document.getElementById("images-container")
+const uploadForm = document.getElementById("upload-form")
+const renderImage = (imageObject) => {
+    return `
+        <div>
+            <h4>${imageObject.titulo}</h4>
+            <img src=${imageObject.foto} />
+        </div>
+    `
+}
+
+const requestOptions = {
+    method: "GET",
+    redirect: "follow"
+};
+
+const loadImages = () => {
+    fetch("https://sheetdb.io/api/v1/1zdjycnz0gdy2", requestOptions)
+        .then((response) => response.json())
+        .then((images) => {
+            let html = ""
+            for(let i = 0; i<images.length; i++) {
+                html += renderImage(images[i])
+            }
+            console.log(html)
+            imagesContainer.innerHTML = html
+        })
+        .catch((error) => console.error(error));
+}
+
+uploadForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    console.log(e.target.title.value)
+    console.log(e.target.imageBase64.value)
+
+    const raw = JSON.stringify({
+        id: "=ROW()",
+        titulo: e.target.title.value,
+        foto: e.target.imageBase64.value
+    })
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    fetch("https://sheetdb.io/api/v1/1zdjycnz0gdy2", {
+        headers: myHeaders,
+        method: "POST",
+        body: raw
+    }).then((response) => response.json())
+    .then((result) => {
+        console.log(result)
+        loadImages()  
+    })
+    .catch((error) => console.error(error))
+})
+
+loadImages()
